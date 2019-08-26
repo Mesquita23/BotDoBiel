44159502c60ed1dd97ddc8fbc894a9a1dc784246
Mesquita23 <biel.mesquita23@gmial.com>
2019-08-26 16:23:35 -0300

    t

--- bot.js
+++ bot.js
@@ -3,6 +3,19 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const stream = fs.createReadStream('./Marilene.mp3');
 
+var opus = require('node-opus');
+
+// Create the encoder.
+// Specify 48kHz sampling rate and 10ms frame size.
+// NOTE: The decoder must use the same values when decoding the packets.
+var rate = 48000;
+var encoder = new opus.OpusEncoder( rate );
+
+// Encode and decode.
+var frame_size = rate/100;
+var encoded = encoder.encode( buffer, frame_size );
+var decoded = encoder.decode( encoded, frame_size );
+
 const client = new Discord.Client();
 
  
